5e005ce10441de72a22fbda43c55a650890847ed
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-12 22:59:06 +0530

    file handling

--- fileHandling.c
+++ fileHandling.c
@@ -1,17 +1,32 @@
-#include<stdio.h>
-
-
+#include <stdio.h>
 
 int main()
 {
-    FILE *ptr =NULL;
-    ptr = fopen("myfile.txt","r");
-    // char c = fgetc(ptr);
+    FILE *ptr = NULL;
+    ptr = fopen("myfile.txt", "r");
+    char c;
+    if (NULL == ptr)
+    {
+        printf("file can't be opened \n");
+    }
+
+    printf("content of this file are \n");
+    do
+    {
+        c = fgetc(ptr);
+
+        printf("%c", c);
+
+    } while (c != EOF);
     // printf("the character I read was %c\n",c);
     //  c = fgetc(ptr);
     // printf("the character I read was %c\n",c);
-    //fgets
-    char str[23];
-    fgets(str,3,ptr);
+    // fgets
+    // char str[23];
+    // fgets(str,16,ptr);
+    // printf("the string I read was %s\n",str);
+    // fputc('s',ptr);
+    // fputs(" it is our duty to inform  you about this",ptr);
+    fclose(ptr);
     return 0;
 }
